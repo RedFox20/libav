1139a59dad5afe3ba570d5d4cfb35bfd5caa3a24
Jorma Rebane <jorma.rebane@gmail.com>
2020-02-06 21:32:25 +0000

    linux build

--- mamafile.py
+++ mamafile.py
@@ -14,15 +14,16 @@ class libav(mama.BuildTarget):
         pass
 
     def package(self):
-        if self.config.is_target_arch_x86():
-            self.export_include('x86/include')
-            self.export_libs('x86/bin', ['.lib','.dll'], src_dir=True)
-        elif self.config.is_target_arch_x64():
-            self.export_include('x64/include')
-            self.export_libs('x64/bin', ['.lib','.dll'], src_dir=True)
-        else:
-            self.export_syslib('avcodec', 'libav-tools')
-            self.export_syslib('avformat', 'libav-tools')
-            self.export_syslib('avutil', 'libav-tools')
+        if self.windows:
+          if self.config.is_target_arch_x86():
+              self.export_include('x86/include')
+              self.export_libs('x86/bin', ['.lib','.dll'], src_dir=True)
+          elif self.config.is_target_arch_x64():
+              self.export_include('x64/include')
+              self.export_libs('x64/bin', ['.lib','.dll'], src_dir=True)
+        elif self.linux:
+            self.export_syslib('libavcodec', 'ffmpeg')
+            self.export_syslib('libavformat', 'ffmpeg')
+            self.export_syslib('libavutil', 'ffmpeg')
 
 
